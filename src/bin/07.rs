98136755af092d566513a52734ecb17af7b7c7e5
Truls <truls.stenrud@gmail.com>
2023-12-12 20:25:19 +0100

    Solve 7

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -2,37 +2,6 @@ use std::{collections::HashMap, fmt::Display, time::Instant};
 
 advent_of_code::solution!(7);
 
-
-fn five_of_a_kind(input: &str)->Option<char>{
-    let mut chars = input.chars();
-    let first = chars.next().unwrap();
-    for i in chars{
-        if i != first{
-            return None;
-        }
-    }
-    Some(first)
-}
-
-// struct Five{
-//     value:char
-// }
-
-// struct Four{
-//     value:char
-// }
-// struct Three{
-//     value:char
-// }
-
-// struct Pair{
-//     value:char
-// }
-// struct TwoPair{
-//     value1:char,
-//     value2:char
-// }
-
     static FIVE:u32 = 6;
     static FOUR:u32 = 5;
     static HOUSE:u32 = 4;
@@ -41,6 +10,18 @@ fn five_of_a_kind(input: &str)->Option<char>{
     static PAIR:u32=1;
     static NOTHING:u32=0;
 
+    fn number_to_hand(c: &u32)->String{
+        match c {
+            6 => "FIVE".to_string(),
+            5 => "FOUR".to_string(),
+            4 => "HOUSE".to_string(),
+            3 => "THREE".to_string(),
+            2 => "TWOPAIRS".to_string(),
+            1 => "PAIR".to_string(),
+            0 => "NOTHING".to_string(),
+                _=> panic!(),
+        }
+    }
     
     fn get_number(c: &char)->usize{
         match c {
@@ -60,6 +41,25 @@ fn five_of_a_kind(input: &str)->Option<char>{
                 _=> panic!(),
         }
     }
+    fn get_number_joker(c: &char)->usize{
+        match c {
+                'A'=> 12,
+                'K'=> 11,
+                'Q'=> 10,
+                'T'=> 9,
+                '9'=> 8,
+                '8'=> 7,
+                '7'=> 6,
+                '6'=> 5,
+                '5'=> 4,
+                '4'=> 3,
+                '3'=> 2,
+                '2'=> 1,
+                'J'=> 0,
+                _=> panic!(),
+        }
+    }
+
 fn find_hand(input: &str)->u32{
 
     let mut counts = [0; 13];
@@ -89,122 +89,175 @@ fn find_hand(input: &str)->u32{
     NOTHING
 }
 
+fn find_hand_joker(input: &str)->u32{
+
+    let mut counts = [0; 13];
+
+    input.chars().for_each(|c| counts[get_number_joker(&c)]+=1);
+
+    let jokers = counts[get_number_joker(&'J')];
+
+    if jokers == 4{
+        return FIVE;
+    }
+
+    if jokers == 3{
+        if counts.contains(&2){
+            return FIVE;
+        }
+        else {
+            return FOUR
+        }
+    }
+
+    if jokers == 2{
+        if counts.contains(&3){
+            return FIVE;
+        }
+        else if counts.iter().filter(|n| *n == &(2)).count() == 2 {
+            return FOUR
+        }
+        else {
+            return THREE
+        }
+    }
+
+    if jokers == 1{
+        if counts.contains(&4){
+            return FIVE;
+        }
+        else if counts.contains(&3){
+            return FOUR;
+        }
+        else if counts.contains(&2){
+            if counts.iter().filter(|n| *n == &(2)).count() == 2 {
+                return HOUSE
+            }
+            else {
+                return THREE
+            }
+        }
+        else {
+            return PAIR
+        }
+    }
+
+
+    if counts.contains(&3) {
+        if counts.contains(&(2)){
+            return HOUSE;
+        }else {
+            return THREE;
+        }
+    }
+    if counts.contains(&(2)){
+        if counts.iter().filter(|n| *n == &(2)).count() == 2{
+            return TWOPAIRS
+        }else{
+            return PAIR
+        }
+    }
+    if counts.contains(&(4)){
+        return FOUR;
+    }
+    if counts.contains(&(5)){
+        return FIVE;
+    }
+    
+    NOTHING
+}
+
 
 struct Hand{
-    cards: String,
+    // cards: String,
     bid: u32,
     value: u32
 }
 
-fn calc_value(cards: &str )->u32{
+fn calc_value_for_highcard_comparison(cards: &str )->u32{
     cards.chars().enumerate().map(|(i, c)|{
-        ((cards.len()-i) as u32)*13 * get_number(&c) as u32
+        (13 as u32).pow((cards.len()-i-1) as u32) * (get_number(&c)+1) as u32
     }).sum()
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    // let mut start = Instant::now();
-    
-    let mut a:[Vec<Hand>;7] = Default::default();
+fn calc_value_for_highcard_comparison_joker(cards: &str )->u32{
+    cards.chars().enumerate().map(|(i, c)|{
+        (13 as u32).pow((cards.len()-i-1) as u32) * (get_number_joker(&c)+1) as u32
+    }).sum()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {    
+    let mut hands:[Vec<Hand>;7] = Default::default();
     
     input.lines().for_each(|line| {
         let mut words = line.split_whitespace();
         let hand = words.next().unwrap();
         let bid = words.next().unwrap().parse::<u32>().unwrap();
         let value_of_hand = find_hand(hand);
-        a[value_of_hand as usize].push(Hand{
-            cards: hand.to_string(), bid: bid, value: calc_value(hand)
+        hands[value_of_hand as usize].push(Hand{
+            // cards:hand.to_string(), 
+            bid: bid, 
+            value: calc_value_for_highcard_comparison(hand)
         })
     });
-    // let mut duration = start.elapsed();
-
-    // println!("Time elapsed in finding hands is: {:?}", duration);
-    // start = Instant::now();
-
-    (0..a.len()).for_each(|i|{
-    a[i].sort_by(| hand_a, hand_b|{
+    
+    (0..hands.len()).for_each(|i|{
+    hands[i].sort_by(| hand_a, hand_b|{
                 if hand_a.value > hand_b.value {
-                    println!("{} is greater", hand_a.cards);
-                    println!("{}", hand_b.cards);
                     return std::cmp::Ordering::Greater;
                 }
                 else if hand_a.value < hand_b.value{
-                    println!("{} is less", hand_a.cards);
-                    println!("{}", hand_b.cards);
                     return std::cmp::Ordering::Less;
                 }
             std::cmp::Ordering::Less
     });
     });
    
-    // duration = start.elapsed();
-
-    // println!("Time elapsed in sorting: {:?}", duration);
     let mut i = 0;
 
-    Some(a.iter().map(|list|{
+    Some(hands.iter().map(|list|{
         list.iter().map(|thing|{
             i+=1;
-            thing.bid * (i+1) as u32
+            thing.bid * (i) as u32
         }).sum::<u32>()
     }).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     
-    let card_map: HashMap<char, u32> = HashMap::from([
-        ('A', 13),
-         ('K', 12),
-         ('Q', 11),
-         ('T', 9),
-         ('9', 8),
-         ('8', 7),
-         ('7', 6),
-         ('6', 5),
-         ('5', 4),
-         ('4', 3),
-         ('3', 2),
-         ('2', 1),
-         ('J', 0),
-    ]);
-
-    let mut hands_and_values:Vec<(String, u32, u32)> = input.lines().map(|line| {
+    let mut hands:[Vec<Hand>;7] = Default::default();
+    
+    input.lines().for_each(|line| {
         let mut words = line.split_whitespace();
         let hand = words.next().unwrap();
         let bid = words.next().unwrap().parse::<u32>().unwrap();
-        let valueOfHand = find_hand(hand);
-        (hand.to_string(), valueOfHand, bid)
-    }).collect();
-
-    hands_and_values.sort_by(| (handA, valueA, bidA), (handB, valueB, bidB)|{
-        if valueA > valueB {
-            std::cmp::Ordering::Greater
-        }
-        else if valueB > valueA{
-            std::cmp::Ordering::Less
-        }
-        else {
-            let chars_a:Vec<char> = handA.chars().collect();
-            let chars_b:Vec<char> = handB.chars().collect();
-            for i in 0..5{
-                let a = card_map[&chars_a[i]];
-                let b = card_map[&chars_b[i]];
-                if a > b {
+        let value_of_hand = find_hand_joker(hand);
+        hands[value_of_hand as usize].push(Hand{
+            // cards: hand.to_string(), 
+            bid: bid, 
+            value: calc_value_for_highcard_comparison_joker(hand)
+        })
+    });
+    
+    (0..hands.len()).for_each(|i|{
+    hands[i].sort_by(| hand_a, hand_b|{
+                if hand_a.value > hand_b.value {
                     return std::cmp::Ordering::Greater;
                 }
-                else if b > a{
+                else if hand_a.value < hand_b.value{
                     return std::cmp::Ordering::Less;
                 }
-            }
-
             std::cmp::Ordering::Less
-        }
-        
     });
+    });
+   
+    let mut i = 0;
 
-    Some(hands_and_values.iter().enumerate().map(|(i, thing)|{
-        // println!("{}", thing.0);
-        thing.2 * (i+1) as u32
+    Some(hands.iter().map(|list|{
+        list.iter().map(|thing|{
+            i+=1;
+            thing.bid * (i) as u32
+        }).sum::<u32>()
     }).sum())
 }
 
@@ -221,16 +274,30 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5905));
     }
 
     #[test]
     fn test_part() {
-//         J9999 is greater
-// Q8888
-        let result1 = calc_value("J9999");
-        let resul2t = calc_value("Q8888");
-        println!("{}", result1);
-        println!("{}", resul2t);
+        
+        [
+            "77977",
+            "JT7T3",
+            "QQ58J",
+            "J3762",
+            "6KTAJ",
+            "24J29",
+            "6689J",
+            "2J222",
+            "JJJJJ",
+            "653JK",
+            "4K5JA",
+            "5593J",
+            "5A95J",
+            "KKJJK",
+            ].iter().for_each( |it|{   
+            println!("{} {}", it, number_to_hand(&find_hand_joker(it)));
+        });
+
     }
 }
